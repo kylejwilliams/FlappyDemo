edd872ef6817a30a535bd46c8bfd01f76b35f2dc
Kyle Williams <kjwkc3@mail.umsl.edu>
2015-12-27 23:17:32 -0600

    Created a play state.
    Created a play state where the actual game takes place. Currently just displays the bird texture. MenuState now transitions to the play state upon receiving a mouse click.

--- core/src/com/kwilliams/game/States/MenuState.java
+++ core/src/com/kwilliams/game/States/MenuState.java
@@ -1,5 +1,6 @@
 package com.kwilliams.game.States;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kwilliams.game.FlappyDemo;
@@ -18,12 +19,15 @@ public class MenuState extends State {
 
     @Override
     public void handleInput() {
-
+        if (Gdx.input.justTouched()) {
+            gsm.set(new PlayState(gsm));
+            dispose();
+        }
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
     }
 
     @Override
@@ -32,5 +36,11 @@ public class MenuState extends State {
         sb.draw(background, 0, 0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
         sb.draw(playBtn, (FlappyDemo.WIDTH / 2) - (playBtn.getWidth() / 2), (FlappyDemo.HEIGHT / 2));
         sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playBtn.dispose();
     }
 }
---
+++ core/src/com/kwilliams/game/States/PlayState.java
@@ -0,0 +1,42 @@
+package com.kwilliams.game.States;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.kwilliams.game.FlappyDemo;
+
+/**
+ * Created by Kyle on 12/27/2015.
+ */
+public class PlayState extends State {
+    private Texture bird;
+
+    public PlayState(GameStateManager gsm) {
+        super(gsm);
+        bird = new Texture("bird.png");
+        cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    protected void update(float dt) {
+        handleInput();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
+        sb.begin();
+        sb.draw(bird, 50, 50);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
