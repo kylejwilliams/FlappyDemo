7e04fe3999fae641a3db4329c7b958975900b637
Kyle Williams <kjwkc3@mail.umsl.edu>
2015-12-28 00:02:00 -0600

    Implemented obstacles
    We now have tubes that the bird will have to navigate through. Tube's positions are generated randomly.

--- core/src/com/kwilliams/game/States/PlayState.java
+++ core/src/com/kwilliams/game/States/PlayState.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kwilliams.game.FlappyDemo;
 import com.kwilliams.game.sprites.Bird;
+import com.kwilliams.game.sprites.Tube;
 
 /**
  * Created by Kyle on 12/27/2015.
@@ -12,12 +13,14 @@ import com.kwilliams.game.sprites.Bird;
 public class PlayState extends State {
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -39,7 +42,9 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
-        sb.end();
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBottomTube().x, tube.getPosBottomTube().y);
+                sb.end();
     }
 
     @Override
---
+++ core/src/com/kwilliams/game/sprites/Tube.java
@@ -0,0 +1,45 @@
+package com.kwilliams.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+/**
+ * Created by Kyle on 12/27/2015.
+ */
+public class Tube {
+    private static final int FLUCTUATION = 130;
+    private static final int TUBE_GAP = 100;
+    private static final int LOWEST_OPENING = 120;
+    private Texture topTube;
+    private Texture bottomTube;
+    private Vector2 posTopTube;
+    private Vector2 posBottomTube;
+    private Random rand;
+
+    public Tube(float x) {
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+        rand = new Random();
+
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBottomTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBottomTube() {
+        return posBottomTube;
+    }
+}
