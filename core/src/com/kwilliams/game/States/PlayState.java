bfc65b8442ab9124650b800132ad4bfb90024c89
Kyle Williams <kjwkc3@mail.umsl.edu>
2015-12-27 23:43:01 -0600

    Created the Bird class
    The bird class encapsulates the birds in the game with their various properties. It currently only drops off the screen.

--- core/src/com/kwilliams/game/States/PlayState.java
+++ core/src/com/kwilliams/game/States/PlayState.java
@@ -4,34 +4,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kwilliams.game.FlappyDemo;
+import com.kwilliams.game.sprites.Bird;
 
 /**
  * Created by Kyle on 12/27/2015.
  */
 public class PlayState extends State {
-    private Texture bird;
+    private Bird bird;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
+        bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
     }
 
     @Override
     protected void handleInput() {
-
     }
 
     @Override
     protected void update(float dt) {
         handleInput();
+        bird.update(dt);
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
     }
 
